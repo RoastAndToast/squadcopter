466db3c4e39a4928a4b2d39189b95b00aacf9157
Max <maksims.terjohins@edu.rtu.lv>
2021-03-24 00:04:52 +0200

    Added Videostream from vision sensor

--- python/simpleTest.py
+++ python/simpleTest.py
@@ -11,9 +11,12 @@
 
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from cv2 import cv2
+import array
 import sys
 import numpy as np
 import math
+import time
 
 try:
     import sim
@@ -26,34 +29,66 @@ except:
     print ('--------------------------------------------------------------')
     print ('')
 
-import time
+# Lucas Kanade parameters
+lk_params = dict(winSize = (25,25),
+                maxLevel = 2,
+                criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
-def retrieveImage():
+
+
+def streamVisionSensor():
+
+    # Mouse function
+    def select_point(event,x,y, flags, params): 
+        global point, point_selected, old_points
+        if event == cv2.EVENT_LBUTTONDOWN:
+            point = (x,y)
+            point_selected = True
+            old_points = np.array([[x,y]], dtype = np.float32)
+            print("TRU")
+
+    cv2.namedWindow('frame')
+
+    point_selected = False
+    point = ()
+    old_points = np.array([[]])    
     #Get the handle of vision sensor
-    global errorCode 
     errorCode,visionSensorHandle = sim.simxGetObjectHandle(clientID,'Vision_sensor',sim.simx_opmode_oneshot_wait)
+    #Get the image
+    errorCode,resolution,image = sim.simxGetVisionSensorImage(clientID,visionSensorHandle,0,sim.simx_opmode_streaming)
+    time.sleep(0.5)
 
-    fig = plt.figure()
-    sensorImage = readDataFromVisionSensor(visionSensorHandle)
-    global img
-    img = plt.imshow(sensorImage,origin='lower', animated = True)
-    #plt.imshow(sensorImage,origin='lower')
-    ani = animation.FuncAnimation(fig, updateFig(sensorImage), interval=30, blit=True)
+    errorCode,resolution,image = sim.simxGetVisionSensorImage(clientID,visionSensorHandle,0,sim.simx_opmode_buffer)
+    sensorImage = np.array(image,dtype=np.uint8)
+    sensorImage.resize([resolution[1],resolution[0],3])
+    old_image = sensorImage.copy()
 
-    plt.show()
+    while (sim.simxGetConnectionId(clientID)!=-1): 
+        cv2.setMouseCallback('frame', select_point)
 
-def readDataFromVisionSensor(Handle):
-    #Get the image of vision sensor
-    errorCode,resolution,image = sim.simxGetVisionSensorImage(clientID,Handle,0,sim.simx_opmode_streaming)
-    time.sleep(0.1)
-    errorCode,resolution,image = sim.simxGetVisionSensorImage(clientID,Handle,0,sim.simx_opmode_buffer)
-    sensorImage = []
-    sensorImage = np.array(image,dtype = np.uint8)
-    sensorImage.resize([resolution[0],resolution[1],3])
-    return sensorImage
+        #Get the image of the vision sensor
+        errorCode,resolution,image = sim.simxGetVisionSensorImage(clientID,visionSensorHandle,0,sim.simx_opmode_buffer)
+        #Transform the image so it can be displayed using pyplot
+        sensorImage = np.array(image,dtype=np.uint8)
+        sensorImage.resize([resolution[1],resolution[0],3])
+        displayedImage = cv2.resize(sensorImage, (480,480))
 
-def updateFig(sensorImage):
-    img.set_array(sensorImage)
+        if point_selected is True:
+            print ("coco")
+            new_points, status, error = cv2.calcOpticalFlowPyrLK(old_image, sensorImage, old_points, None, **lk_params)
+            old_image = sensorImage.copy() #current frame becomes previous
+            old_points = new_points #current x,y points become previous
+            x,y = new_points.ravel()
+            cv2.circle(sensorImage, (x,y),8, (0,0,0),-1)
+            
+
+        cv2.imshow('frame',displayedImage)
+        if cv2.waitKey(30) & 0xFF == ord('q'):
+            break
+    cv2.destroyAllWindows()    
+    print ('End of Simulation')
+
+
 
 print ('Program started')
 sim.simxFinish(-1) # just in case, close all opened connections
@@ -70,13 +105,9 @@ if clientID!=-1:
 
     time.sleep(2)
 
-    sim.simxAddStatusbarMessage(clientID,'Hello CoppeliaSim!',sim.simx_opmode_oneshot)
+    #sim.simxAddStatusbarMessage(clientID,'Hello CoppeliaSim!',sim.simx_opmode_oneshot)
 
-    while(True):
-        retrieveImage()
-        time.sleep(0.03)
-        #if 0xFF == ord('q'): # delay between frames 30ms, stop if 'q' pressed
-        #    break 
+    streamVisionSensor()
     
 
     # Before closing the connection to CoppeliaSim, make sure that the last command sent out had time to arrive. You can guarantee this with (for exaplte):
